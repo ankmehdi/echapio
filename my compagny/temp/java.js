12fbf6390f447b71df8fa6ea3e628a869f9889b6
ankmehdi <mehdi.ankouri.atsl@gmail.com>
2025-02-26 16:11:54 -0500

    java.js

--- my compagny/temp/java.js
+++ my compagny/temp/java.js
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
 <html>
-<body>
-
-<h2>JavaScript Statements</h2>
-
-<p>JavaScript code blocks are written between { and }</p>
-
-<button type="button" onclick="myFunction()">1</button>
-<button onclick="myFunction()">2</button>
-<button onclick="myFunction()">3</button>
-<button onclick="myFunction()">4</button>
-<button onclick="myFunction()">5</button>
-<button onclick="myFunction()">6</button>
-<button onclick="myFunction()">7</button>
-<button onclick="myFunction()">8</button>
-<button onclick="myFunction()">9</button>
-<button onclick="myFunction()">10</button>
-<button onclick="myFunction()">11</button>
-<button onclick="myFunction()">12</button>
-<p id="demo1"></p>
-<p id="demo2"></p>
-
+<head>
 <script>
 function myFunction() {
-  document.getElementById("demo1").innerHTML = "Hello Dolly!";
-  document.getElementById("demo2").innerHTML = "How are you?";
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 </script>
+</head>
+<body>
+<h2>Demo JavaScript in Head</h2>
+
+<p id="demo">A Paragraph</p>
+<button type="button" onclick="myFunction()">Try it</button>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<button type="button" onclick="myFunction()"> try it</button>
 
 </body>
 </html>
