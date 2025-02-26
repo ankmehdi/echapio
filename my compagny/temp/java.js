bf484d5732418f6b6e80cd8e4a3f9e11f97954a1
ankmehdi <mehdi.ankouri.atsl@gmail.com>
2025-02-26 16:12:57 -0500

    java.js

--- my compagny/temp/java.js
+++ my compagny/temp/java.js
@@ -11,23 +11,23 @@ function myFunction() {
 <h2>Demo JavaScript in Head</h2>
 
 <p id="demo">A Paragraph</p>
-<button type="button" onclick="myFunction()">Try it</button>
+<button type="button" onclick="myFunction()">1</button>
 
-<button type="button" onclick="myFunction()">Try it</button>
+<button type="button" onclick="myFunction()">2</button>
 
-<button type="button" onclick="myFunction()">Try it</button>
+<button type="button" onclick="myFunction()">3</button>
 
-<button type="button" onclick="myFunction()">Try it</button>
+<button type="button" onclick="myFunction()">4</button>
 
-<button type="button" onclick="myFunction()">Try it</button>
+<button type="button" onclick="myFunction()">5</button>
 
-<button type="button" onclick="myFunction()">Try it</button>
+<button type="button" onclick="myFunction()">6</button>
 
-<button type="button" onclick="myFunction()">Try it</button>
+<button type="button" onclick="myFunction()">7</button>
 
-<button type="button" onclick="myFunction()">Try it</button>
+<button type="button" onclick="myFunction()">8</button>
 
-<button type="button" onclick="myFunction()"> try it</button>
+<button type="button" onclick="myFunction()"> 9</button>
 
 </body>
 </html>
