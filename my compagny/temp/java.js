85457b687dbc74840e7c403e3baeb582cf4a0fe4
ankmehdi <mehdi.ankouri.atsl@gmail.com>
2025-02-27 11:11:59 -0500

    Update java.js

--- my compagny/temp/java.js
+++ my compagny/temp/java.js
@@ -8,26 +8,3 @@ function myFunction() {
 <h2>Demo JavaScript in Head</h2>
 
 <p id="demo">A Paragraph</p>
-<button type="button" onclick="myFunction()">1</button>
-
-<button type="button" onclick="myFunction()">2</button>
-
-<button type="button" onclick="myFunction()">3</button>
-
-<button type="button" onclick="myFunction()">4</button>
-
-<button type="button" onclick="myFunction()">5</button>
-
-<button type="button" onclick="myFunction()">6</button>
-
-<button type="button" onclick="myFunction()">7</button>
-
-<button type="button" onclick="myFunction()">8</button>
-
-<button type="button" onclick="myFunction()"> 9</button>
-
-<button type="button" onclick="myFunction()">10</button>
-
-<button type="button" onclick="myFunction()">11</button>
-
-
