0834629b5a1df3a01e58c7b4d70f9ee2daf513da
ankmehdi <mehdi.ankouri.atsl@gmail.com>
2025-02-27 12:13:38 -0500

    Update java.js

--- my compagny/temp/java.js
+++ my compagny/temp/java.js
@@ -1,10 +1,4 @@
 
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
+  .";
 }
-</script>
-</head>
-<body>
-<h2>Demo JavaScript in Head</h2>
-
-<p id="demo">A Paragraph</p>
